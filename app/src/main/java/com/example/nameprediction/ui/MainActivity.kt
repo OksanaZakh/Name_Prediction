f956bcf597a1898c53ab881696f65eb3c4cb1de4
ozakharchenko <v-ozakharchenko@vrbo.com>
2021-11-17 18:11:04 +0200

    Initial commit

---
+++ app/src/main/java/com/example/nameprediction/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.nameprediction.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.nameprediction.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
